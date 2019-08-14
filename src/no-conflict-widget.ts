7ab2f812630dc4ef08305608aba5bfd2f93938b7
Matthew Wilkes <tonicblue@gmail.com>
2019-08-14 11:31:13 +0100

    Moved all no-conflict stuff into wrapper. Removed jQuery, fixed iframe resizing issue

--- src/no-conflict-widget.ts
+++ src/no-conflict-widget.ts
@@ -1,5 +1,4 @@
-import * as $ from 'jquery';
-(window as any).$ = $;
+require('./widget-loading.scss');
 
 export class NoConflictWidget {
   private _currentLocation: string|undefined;
@@ -13,8 +12,8 @@ export class NoConflictWidget {
     this._currentLocation = value.startsWith('#') ? value.substr(1) : value;
   }
 
-  me: JQuery<HTMLElement>;
-  baseElement: JQuery<HTMLElement>;
+  baseElement: HTMLIFrameElement;
+  loadingElement: HTMLDivElement;
   frameContentWindow: Window;
   frameDocument: Document;
   htmlTemplate = `<!doctype html>
@@ -29,56 +28,98 @@ export class NoConflictWidget {
         padding: 0;
         overflow: hidden;
       }
+
+      .scvo-widget-loading-indicator {
+        display: none !important;
+      }
     </style>
   </head>
   <body>
     %SCRIPT_TAG%
     <script>
-      window.addEventListener('hashchange', function() {
-        window.parent.postMessage({ event: 'hashchange', data: window.location.hash }, '%ORIGIN%');
-      });
-
       var oldHeight = 0;
+      var oldHash = window.location.hash;
+      var oldLoading = null;
+
       var clock = window.requestAnimationFrame(tick);
       function tick() {
         window.cancelAnimationFrame(clock);
 
-        var body = document.body, html = document.documentElement;
-        var newHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-        if (oldHeight !== newHeight) {
-          window.parent.postMessage({ event: 'resized', data: newHeight });
-          oldHeight = newHeight;
-        }
+        checkHeight();
+        checkHash();
+        checkLoading();
 
         clock = window.requestAnimationFrame(tick);
       }
 
+      function checkHeight() {
+        var widgetElement = document.querySelector('.scvo-widget');
+        if (widgetElement) {
+          var newHeight = widgetElement.offsetHeight + 30;
+          if (oldHeight !== newHeight) {
+            var eventData = {
+              event: 'height-change',
+              data: newHeight
+            }
+            window.parent.postMessage(eventData, '%ORIGIN%');
+            oldHeight = newHeight;
+          }
+        }
+      }
+
+      function checkHash() {
+        var newHash = window.location.hash;
+        if (oldHash !== newHash) {
+          var eventData = {
+            event: 'hash-change',
+            data: newHash
+          }
+          window.parent.postMessage(eventData, '%ORIGIN%');
+          oldHash = newHash;
+        }
+      }
+
+      function checkLoading() {
+        var widgetElement = document.querySelector('.scvo-widget');
+        if (widgetElement) {
+          var newLoading = widgetElement.classList.contains('scvo-widget-loading') ? 'loading' : 'not-loading';
+          if (oldLoading !== newLoading) {
+            var eventData = {
+              event: 'loading-change',
+              data: newLoading
+            };
+            window.parent.postMessage(eventData, '%ORIGIN%');
+            oldLoading = newLoading;
+          }
+        }
+      }
     </script>
   </body>
 </html>`;
 
-  constructor (me: HTMLScriptElement) {
-    this.me = $(me);
-    this.baseElement = $('<iframe>')
-      .attr('border', '0')
-      .css({
-        'margin': '0',
-        'border': '0',
-        'width': '100%',
-        'height': '300px'
-      })
-      .insertAfter(this.me);
+  constructor (private me: HTMLScriptElement) {
+    this.baseElement = document.createElement('iframe');
+    this.baseElement.setAttribute('border', '0');
+    this.baseElement.style.margin = '0';
+    this.baseElement.style.border = '0';
+    this.baseElement.style.width = '100%';
+    this.baseElement.style.height = '300px';
+    this.me.insertAdjacentElement('afterend', this.baseElement);
+
+    this.loadingElement = document.createElement('div');
+    this.loadingElement.classList.add('scvo-widget-loading-indicator');
+    this.me.insertAdjacentElement('afterend', this.loadingElement);
 
     window.addEventListener('message', this.handleMessage.bind(this), false);
     window.addEventListener('hashchange', this.handleHashChange.bind(this), false);
 
-    const meTagHtml = this.me[0].outerHTML;
+    const meTagHtml = this.me.outerHTML;
     const scriptTagHtml = meTagHtml.replace('no-conflict-', '');
     const frameHtml = this.htmlTemplate
       .replace(/%SCRIPT_TAG%/ig, scriptTagHtml)
       .replace(/%ORIGIN%/ig, location.origin);
 
-    this.frameContentWindow = (this.baseElement[0] as HTMLIFrameElement).contentWindow as Window;
+    this.frameContentWindow = this.baseElement.contentWindow as Window;
 
     this.frameDocument = this.frameContentWindow.document;
     this.frameDocument.open();
@@ -91,20 +132,17 @@ export class NoConflictWidget {
   handleMessage(message: any) {
     if (message.source === this.frameContentWindow) {
       switch (message.data.event) {
-        case ('hashchange'):
+        case ('hash-change'):
           console.log('Window message => Hash change:', message.data.data.substr(1));
           window.location.hash = message.data.data.substr(1);
           break;
-        case ('resized'):
-          console.log('Window message => Frame resize:', message.data.data);
-          this.baseElement.css('height', message.data.data);
+        case ('height-change'):
+          console.log('Window message => Height change:', message.data.data);
+          this.baseElement.style.height = message.data.data + 'px';
           break;
-        case ('navigationStart'):
-          console.log('Window message => Navigation start:', message.data.data);
-          this.baseElement.css('height', '100vh');
-          break;
-        case ('navigationEnd'):
-          console.log('Window message => Navigation end:', message.data.data);
+        case ('loading-change'):
+          console.log('Window message => Loading change:', message.data.data);
+          this.loadingElement.style.display = message.data.data === 'loading' ? 'block' : 'none';
           break;
         default:
           console.log('Unknown event:', message.data);
